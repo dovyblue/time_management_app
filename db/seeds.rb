d9c6805e61004aff8ff40dbc437d339bf8b61e40
Dov Bluming <dovybluming@gmail.com>
2017-05-23 15:58:40 -0400

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = Category.create([{name: "Housework", icon: "fa fa-home"}, {name: "Personal", icon: "glyphicon glyphicon-user"}, {name: "Errands", icon: "fa fa-shopping-bag"}, {name: "Miscellaneous", icon: "glyphicon glyphicon-pushpin"}])
+
